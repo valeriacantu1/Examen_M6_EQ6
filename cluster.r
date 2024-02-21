0de4a0e268d084fe8d0a3b1de76e6fccaeb529dc
emilio mtz <a01177730@tec.mx>
2024-02-20 19:32:09 -0600

    Pasos 1-3

--- cluster.r
+++ cluster.r
@@ -1,1 +1,25 @@
 #Valeria Cantu a01570758
+#Emilio Martinez a01177730
+
+# Paso 1 - Instalar librerias y cargarlas
+install.packages("cars")
+library("cars")
+
+install.packages("cluster")
+library(cluster)
+
+install.packages("ggplot2")
+library("ggplot2")
+
+install.packages("data.table")
+library("data.table")
+
+install.packages("factoextra")
+library("factoextra")
+
+# Paso 2 - Determinar cantidad de Clusters
+grupos <- 4
+
+# Paso 3 - Utilizar KNN para determinar los clusters
+segmentos <- kmeans(cars, grupos)
+segmentos
